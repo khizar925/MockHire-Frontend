59067dc41f59224d3c9362074b6d976fe4588ff5
khizar925 <khizarqamar06@gmail.com>
2025-08-07 11:14:45 +0500

    constants.js added

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+export const interviewCovers = [
+  "/covers/adobe.png",
+  "/covers/amazon.png",
+  "/covers/facebook.png",
+  "/covers/hostinger.png",
+  "/covers/pinterest.png",
+  "/covers/quora.png",
+  "/covers/reddit.png",
+  "/covers/skype.png",
+  "/covers/spotify.png",
+  "/covers/telegram.png",
+  "/covers/tiktok.png",
+  "/covers/yahoo.png",
+];
