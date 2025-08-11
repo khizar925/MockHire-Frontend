154efe0d5cb1b4fd84f3f30e3105fe62800dd94c
khizar925 <khizarqamar06@gmail.com>
2025-08-11 22:20:38 +0500

    Layout update for clerk params fix

--- src/Pages/Layout.jsx
+++ src/Pages/Layout.jsx
@@ -1,8 +1,32 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
 
 export default function Layout() {
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const params = new URLSearchParams(location.search);
+        const handshakeParams = [
+            "__clerk_handshake",
+            "__clerk_redirect_url",
+            "__clerk_session_id"
+        ];
+        let changed = false;
+
+        handshakeParams.forEach((p) => {
+            if (params.has(p)) {
+                params.delete(p);
+                changed = true;
+            }
+        });
+
+        if (changed) {
+            navigate(location.pathname, { replace: true });
+        }
+    }, [location, navigate]);
+
     return (
         <div
             className="min-h-screen bg-cover bg-no-repeat bg-center"
